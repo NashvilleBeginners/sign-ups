1a9d51278a20c3655da6d785b87087b48589d151
Sean Hill <sean@reax.io>
2015-06-18 19:27:22 -0500

    Send signup confirmed mail after confirmed_at changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :first_name, :last_name, presence: true
+
+  before_save :send_confirmed_signup_email
+
+  private
+
+  def send_confirmed_signup_email
+    if confirmed_at_changed?
+      UserMailer.confirmed_signup(self).deliver_now
+    end
+  end
 end
